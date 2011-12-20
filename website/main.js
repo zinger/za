2f989c75437baef86f780dfd5c6a65a9355ae6e2
sunil <skmasand@gmail.com>
2011-12-19 22:28:08 -0800

    website alignment etc

--- website/main.js
+++ website/main.js
@@ -6,6 +6,47 @@
   var ShowPage = function(args) {
     var baseWidth=128; //resolution width divided by 8 .... 128
     var baseHeight=96; // res height by 8 .... 96
+    
+    var margin;
+    var contentWidth;
+    if ($(window).width() > 1200) {
+      baseWidth=128;
+      baseHeight=96;
+      contentWidth = 1024;
+      margin = ($(window).width() - 1024 )/ 2;
+      if ($(window).height() > 768) { marginV = ($(window).height() - 768 )/ 2;}
+      else marginV = 0;
+      $('#content').css({height: 768, width: contentWidth, marginLeft:margin, marginTop: marginV, marginBottom: marginV, marginRight: margin});
+    } else if ($(window).width() > 1040) {
+      baseWidth = 104;
+      baseHeight = 78;
+      contentWidth = 832;
+      margin = ($(window).width() - 832 )/ 2;
+      if ($(window).height() > 624) { marginV = ($(window).height() - 624 )/ 2;}
+      else marginV = 0;
+      $('#content').css({height: 624, width: contentWidth, marginLeft:margin, marginTop: marginV, marginBottom: marginV, marginRight: margin});
+    } else {
+      baseWidth = 80;
+      baseHeight = 60;
+      contentWidth = 640;
+      if ($(window).width() > 640) margin = ($(window).width() - 640 )/ 2;
+      else margin = 0;
+      if ($(window).height() > 480) { marginV = ($(window).height() - 480 )/ 2;}
+      else marginV = 0;
+      $('#content').css({height: 480, width: contentWidth, marginLeft:margin, marginTop: marginV, marginBottom: marginV, marginRight: margin});
+    }
+    /*
+    if (screen.width >= 1200) {
+      baseWidth=128;
+      baseHeight=96;
+      $('#content').css({height: 960, width: 1280, marginLeft:128, marginTop: 96, marginBottom: 96, marginRight: 128});
+    } else {
+      baseWidth=100;
+      baseHeight=75;
+      $('#content').css({height: 780, width: 1280, paddingLeft:128, paddingRight: 128});
+
+    }*/
+    
     var width=baseWidth;
     var height=baseHeight;
     var top=0;
@@ -34,7 +75,9 @@
        //     images.push({src: 'images/indian-girls-2-by-2.png'});
       images.push({src: 'images/turkey-2-by-2.png'});
             images.push({src: 'images/mountainbike-4-by-1.png'}); // 4 by 1
-    for (var i=0; i<10; i++) {
+                        images.push({src: 'images/title3.png'}); // 4 by 1
+
+    for (var i=0; i<11; i++) {
       var id = 'div' + i;
       var $div = $('<div id="'+id+'" class="imgDiv"></div>');
       switch (i) {
@@ -108,11 +151,18 @@
           left=baseWidth*4;
           color='purple';
           break;
+        case 10:
+          width=baseWidth*4;
+          height=baseHeight*2;
+          top=baseHeight*3;
+          left=baseWidth*2;
         default:
           break;
       }
       $div.css({backgroundColor:color, top: top, left:left, width:width, height:height});
-      $div.append($('<img src="'+images[i].src+'" />'));
+      var $img = $('<img src="'+images[i].src+'" />');
+      $img.css({top: top, left:left, width:width, height:height});
+      $div.append($img);
       $("#content").append($div);
     };
   };
